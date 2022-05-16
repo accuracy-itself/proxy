01d04aa04f94c3d4f02f5cb4ecf104c918ea6174
Lena Stankevich <stankevich.lenkaa@mail.ru>
2022-05-16 11:01:37 +0300

    does not work

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Proxy
+{
+    class Program
+    {
+        static IPEndPoint? IPEndPointLocal;
+        static IPAddress? IPAddressLocal;
+        const int LocalPort = 8005;
+        const int BufferSize = 65536;
+        static string URL;
+        static void Main()
+        {
+            Console.WriteLine("hey, my dear proxy user!");
+
+            IPAddressLocal = IPAddress.Parse("127.0.0.1");
+            IPEndPointLocal = new IPEndPoint(IPAddressLocal, LocalPort);
+            TcpListener listener = new TcpListener(IPAddressLocal, LocalPort);
+            try
+            {
+                listener.Start();
+                while (true)
+                {
+                    //receiving requests from browser and creating threads for processing them
+                    TcpClient handler = listener.AcceptTcpClient();
+                    Thread ProcessRequestThread = new(() => ProcessRequest(handler));
+                    ProcessRequestThread.Start();
+                    Console.WriteLine("--------------------------------------------------------------------------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("main: \n" + ex.ToString());
+                Console.WriteLine();
+                Console.WriteLine();
+
+                return;
+            }
+            finally
+            {
+
+            }
+        }
+
+        public static void ProcessRequest(TcpClient handler)
+        {
+            NetworkStream browserStream = handler.GetStream();
+            byte[] browserData = new byte[BufferSize];
+
+            try
+            {
+                while (browserStream.CanRead)
+                {
+                    try
+                    {
+                        //read request and get server URL and port
+                        int readbytes = browserStream.Read(browserData);
+                        if (readbytes == 0)
+                            return;
+
+                        string[] data = Encoding.ASCII.GetString(browserData, 0, readbytes).Trim().Split(new char[] { '\r', '\n' });
+                        string hostString = data.FirstOrDefault(s => s.Contains("Host"));
+
+                        hostString = hostString.Substring(hostString.IndexOf(":") + 2);
+                        string[] host = hostString.Trim().Split(':');
+
+                        URL = data[0].Split()[1];
+                        int port;
+                        if (host.Length == 2)
+                            port = Convert.ToInt32(host[1]);
+                        else
+                            port = 80;
+
+                        //create socket and stream for interacting with server
+                        TcpClient serverSender = new TcpClient(host[0], port);
+
+                        NetworkStream serverStream = serverSender.GetStream();
+                        //browserStream.CopyTo(serverStream);
+
+                        try
+                        {
+                            serverStream.Write(browserData, 0, readbytes);
+                            //get response code and copy response to browser stream
+                            byte[] responseBuffer = new byte[BufferSize];
+
+                            int len = serverStream.Read(responseBuffer);
+                            browserStream.Write(responseBuffer, 0, len);
+
+                            string responseString = Encoding.UTF8.GetString(responseBuffer, 0, len);
+                            string[] response = responseString.Trim().Split(new char[] { '\r', '\n' });
+
+                            response = response[0].Trim().Split(' ');
+                            int answerCode = Convert.ToInt32(response[1]);
+                            Console.WriteLine(URL + '-' + answerCode + " " + response[2]);
+
+                            serverStream.CopyTo(browserStream);
+                            serverStream.Close();
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("serverStream:\n" + ex.ToString());
+                            Console.WriteLine();
+                            Console.WriteLine();
+                        }
+                        finally
+                        {
+                            serverStream.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("here: \n" + ex.ToString());
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        return;
+                    }
+
+                }
+            }
+            finally
+            {
+                handler.Close();
+            }
+        }
+    }
+}
